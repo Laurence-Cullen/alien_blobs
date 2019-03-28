4d7f10ae9cc74c576bc885df736c88f04b061077
Greg Bland <gbland94@hotmail.com>
2019-03-28 00:58:54 +0000

    comitting changes

--- agents.py
+++ agents.py
@@ -5,18 +5,19 @@ from keras.layers import Conv2D, BatchNormalization, MaxPooling2D, Dropout, Flat
 
 
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, player_id=None):
         self._name = name
+        self.player_id = player_id
 
-    def play_move(self, board, player_id):
-        pass
-
-    def next_move(self, board, player_id):
+    def next_move(self, board):
         pass
 
     @property
     def name(self):
         return self._name
+
+    def set_id(self, player_id):
+        self.player_id = player_id
 
 
 class RandomPlayer(Player):
@@ -24,7 +25,7 @@ class RandomPlayer(Player):
     Plays on a random clear part of the board.
     """
 
-    def next_move(self, board, player_id):
+    def next_move(self, board):
         return random.choice(board.legal_moves())
 
 
@@ -33,8 +34,8 @@ class ProximityRandomPlayer(Player):
     Plays on a random part of the board that is adjacent to opponent pieces if possible.
     """
 
-    def next_move(self, board, player_id):
-        proximity_moves = board.legal_moves_adjacent_to_player(1 - player_id)
+    def next_move(self, board):
+        proximity_moves = board.legal_moves_adjacent_to_player(1 - self.player_id)
 
         if len(proximity_moves) > 0:
             return random.choice(proximity_moves)
--- game.py
+++ game.py
@@ -1,14 +1,62 @@
+from board import Board
+from agents import RandomPlayer
+
+
 class Game:
 
-    def __init__(self, board, player_one, player_two):
+    def __init__(self, board, player_one, player_two, game_length=20):
         self.board = board
+        self.game_length = game_length
         self.player_one = player_one
+        self.player_one.set_id(0)
         self.player_two = player_two
+        self.player_two.set_id(1)
+        self.game_length = game_length
 
+    def player_score(self, player_id):
+        player_score = 0
+        for [i, j] in [[i, j] for i in range(self.board.board_size) for j in range(self.board.board_size) if
+                       self.board.board[i][j][1 - player_id] == 1]:
+            try:
+                if (self.board.board[i + 1][j][player_id] == 1 and self.board.board[i - 1][j][player_id] == 1 and
+                        self.board.board[i][j + 1][player_id] == 1 and
+                        self.board.board[i][j - 1][player_id] == 1):
+                    player_score += 1
 
+            except IndexError:
+                pass
+
+        return player_score
+
+    @property
+    def player_one_score(self):
+        return self.player_score(self.player_one.player_id)
+
+    @property
+    def player_two_score(self):
+        return self.player_score(self.player_two.player_id)
 
     def play_game(self):
         game_turn = 0
 
+        while game_turn < self.game_length:
+            move = self.player_one.next_move(self.board)
+            self.board.update_board(move, self.player_one.player_id)
+
+            move = self.player_two.next_move(self.board)
+            self.board.update_board(move, self.player_two.player_id)
+
+            game_turn += 1
 
 
+def main():
+    board = Board()
+    player_one = RandomPlayer(name='one')
+    player_two = RandomPlayer(name='two')
+    game = Game(board, player_one, player_two)
+    game.play_game()
+    print(game.player_one_score, game.player_two_score)
+
+
+if __name__ == '__main__':
+    main()
