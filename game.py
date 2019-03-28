f4a33770e7fb943ac9570da55b44feada789e6b6
Laurence-Cullen <laurencesimoncullen@gmail.com>
2019-03-28 14:05:10 +0000

    adding game history and regeneration of board state at any point

--- game.py
+++ game.py
@@ -7,12 +7,21 @@ from board import Board
 class Game:
     def __init__(self, board, player_one, player_two, game_length=20):
         self.board = board
+
+        # record of the sequential moves making up the game in form {move_num: [i, j, player_id]}
+        self._moves = {}
+
+        # tracks how many moves have been made
+        self._move_counter = 0
+
         self.game_length = game_length
         self.player_one = player_one
         self.player_one.set_id(0)
         self.player_two = player_two
         self.player_two.set_id(1)
         self.game_length = game_length
+
+        self.winner = None
 
     def player_score(self, player_id):
         player_score = 0
@@ -53,13 +62,29 @@ class Game:
         game_turn = 0
 
         while game_turn < self.game_length:
-            move = self.player_one.next_move(self.board)
-            self.board.update_board(move, self.player_one.player_id)
-
-            move = self.player_two.next_move(self.board)
-            self.board.update_board(move, self.player_two.player_id)
+            self.play_turn(self.player_one)
+            self.play_turn(self.player_two)
 
             game_turn += 1
+
+    def play_turn(self, player):
+        move = player.next_move(self.board)
+        self.board.update_board(move, player.player_id)
+        self._moves[self._move_counter] = [move[0], move[1], player.player_id]
+        self._move_counter += 1
+
+    def board_at_move(self, move_num):
+        """
+        Reconstruct the board state at a particular move, building the board from the history of
+        moves up to this point.
+        """
+
+        board = Board(board_size=self.board.board_size)
+
+        for i in range(move_num):
+            board.update_board(move=[self._moves[i][0], self._moves[i][1]], player_id=self._moves[i][2])
+
+        return board
 
 
 def main():
@@ -71,6 +96,8 @@ def main():
     game.play_game()
     print(game.board)
     print(game.player_one_score, game.player_two_score)
+
+    print(game.board_at_move(10))
 
 
 if __name__ == '__main__':
